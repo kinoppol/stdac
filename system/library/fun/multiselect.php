d98aa91e9d12ecefabb7fd8f101b1957751a760c
kinoppol <i.noppol@gmail.com>
2019-11-22 17:38:59 +0700

    multiselect

---
+++ system/library/fun/multiselect.php
@@ -0,0 +1,3 @@
+<?php
+print '<link href="'.LIB_PATH.'ext/lou-multi-select/css/multiselect.css" media="screen" rel="stylesheet" type="text/css">';
+print '<script src="'.LIB_PATH.'js/jquery.multi-select.js" type="text/javascript"></script>';
