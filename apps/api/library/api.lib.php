8a3b6c204efa6475a4680d20a27ebefb47fe1b77
kinoppol <kinoppol@hotmail.com>
2019-11-28 22:34:06 +0700

    api

--- apps/api/library/api.lib.php
+++ apps/api/library/api.lib.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(0);
-
 class pSql{
 	public $passphres; 
 	public $url; 
@@ -9,21 +7,44 @@ class pSql{
 		$this->url=$url;	
 	}
 	function query($query,$debug=false){	
-		if($this->passphres){
-		   $key=substr(md5(date('Ydm')),0,4);
+		if($this->passphres=$passphres){
 			$q=openssl_encrypt(base64_encode($query), 'AES-128-CBC', $key,0,$this->passphres.date('Ydm')); 
 		}else{ 
 			$q=urlencode($query); 
 		}
-		$target=$this->url.'?key='.substr(md5(date('Ydm')),0,4).'&q='.$q;
+		$target=$this->url.'?key='.substr(md5(date('Ydm').$key),0,4).'&q='.$q;
 		if($debug)print $target;
 		return json_decode(file_get_contents($target));
 	}
 	function decode($q){
 		
-		if($this->passphres){
+		if($this->passphres=$passphres){
 			$result=base64_decode(openssl_decrypt($q, 'AES-128-CBC', $key,0,$this->passphres.date('Ydm')));
 		}else{
 			$result=preg_replace('/\\\\/', 
 '',urldecode($q));
 		}
+		return $result;
+    }
+}
+
+$key_list=array(
+    'rms'=>array(
+        'key'=>'0dc36f73fbfc102d83a01a521e42cf2d',
+        'function'=>'select',
+    )
+);
+
+
+function ac($q,$api_id,$api_key,$key_list){
+    $q=explode(' ',$q);
+
+    $available_function=explode(',',$key_list[$api_key])
+
+
+}
+
+
+$api_query=new psql(); 
+$query=$api_query->decode($_GET['q']);
+$data=$db->query($query);
