8a907c06896469b120e4ad22a25c527ef1f9d30e
kinoppol <kinoppol@hotmail.com>
2019-12-02 17:57:57 +0700

    Update rms.lib.php

--- apps/admin/library/rms.lib.php
+++ apps/admin/library/rms.lib.php
@@ -1,29 +1,9 @@
 <?php
 error_reporting(0);
 
-class pSql{
-	public $passphres; 
-	public $url; 
-	public function __construct($passphres,$url){	
-		$this->passphres=$passphres; 
-		$this->url=$url;	
-	}
-	function query($query,$debug=false){	
-		if($this->passphres){
-		   $key=substr(md5(date('Ydm')),0,4);
-			$q=openssl_encrypt(base64_encode($query), 'AES-128-CBC', $key,0,$this->passphres.date('Ydm')); 
-		}else{ 
-			$q=urlencode($query); 
-		}
-		$target=$this->url.'?key='.substr(md5(date('Ydm')),0,4).'&q='.$q;
-		if($debug)print $target;
-		return json_decode(file_get_contents($target));
-	}
-	function decode($q){
-		
-		if($this->passphres){
-			$result=base64_decode(openssl_decrypt($q, 'AES-128-CBC', $key,0,$this->passphres.date('Ydm')));
-		}else{
-			$result=preg_replace('/\\\\/', 
-'',urldecode($q));
-		}
+function rms_get_data($rmsurl,$app_name,$data){
+    $target_url=$rmsurl."/api_connection.php?data=".$data."&app_name=".$app_name;
+    $rawdata=file_get_contents($target_url);
+    $json=json_decode($rawdata,true);
+    return $json;
+}
