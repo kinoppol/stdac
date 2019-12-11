3ec564300d0991fcd615c601d107852dd3a661c5
kinoppol <i.noppol@gmail.com>
2019-12-11 19:52:13 +0700

    Update student.php

--- apps/admin/transfer/student.php
+++ apps/admin/transfer/student.php
@@ -9,7 +9,7 @@ foreach($student_data as $student){
     if($student['status']==0){//0=คือสถานะที่นักเรียนกำลังเรียนอยู่
     $data=array(
         "student_id"=>sQ($student['student_id']),
-        "prefix_id"=>sQ($student['perfix_id']),
+        //"prefix_id"=>sQ($student['perfix_id']),
         "gender_id"=>sQ($student['gender_id']),
         "stu_fname"=>sQ($student['stu_fname']),
         "stu_lname"=>sQ($student['stu_lname']),
