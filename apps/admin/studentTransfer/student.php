4c6bb8d54604bd3644e656932a334c33e2ee1f52
kinoppol <kinoppol@hotmail.com>
2020-11-24 00:35:14 +0700

    Update student.php

--- apps/admin/studentTransfer/student.php
+++ apps/admin/studentTransfer/student.php
@@ -1,7 +1,7 @@
 <?php
 $import_student=0;
 sDeleteTb($systemDb,"std");
-$step=100;
+$step=10;
 //print "Hello";
 $rms=get_system_config("rms_url");
 $student_count=rms_get_data($rms,'nutty','student',null,$count="yes");
