0ff99cc61af2497db32d80e14079253da7193752
kinoppol <kinoppol@hotmail.com>
2022-05-23 10:19:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,6 @@ print "กำลังปรับปรุงข้อมูล.. รอสั�
 exit();
 }
 
-load_fun('form');
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 $startRender=microtime(true);
 $systemFoot='';
 $systemTop='';
