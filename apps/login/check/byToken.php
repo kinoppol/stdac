f59a251ed0be3b2410a13b8f91c979b238c21da3
kinoppol <kinoppol@hotmail.com>
2023-09-04 10:11:15 +0700

    rr

--- apps/login/check/byToken.php
+++ apps/login/check/byToken.php
@@ -1,4 +1,6 @@
 <?php
 $id=$_SESSION['id_token'];
 print $id;
-signInUser($id,$remember=false,$noRedirect=false);
+$data=sSelectTb($systemDb,'userdata','id','people_id="'.$id.'" AND active="Y" limit 1');
+$userdata=$data[0];
+signInUser($userdata['id'],$remember=false,$noRedirect=false);
