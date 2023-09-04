f5885ccefdd1328091839b8d76a7c1259b5d7d0c
kinoppol <kinoppol@hotmail.com>
2023-09-04 09:36:09 +0700

    reto

--- token.php
+++ token.php
@@ -5,18 +5,9 @@ $seed=$id.date("Y-m-d");
 $chk_token=md5($seed);
 if($token!=$chk_token){
     print "Error - token expired.";
-    //print $chk_token;
-    //print "<br>".$token;
     exit();
 }else{
-    //require_once('system/include/config.php');
-    require_once('system/library/functions.php');
-    require_once('system/library/fun/user.fun.php');
-    ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
     print "โปรดรอสักครู่..";
-    $id=$_SESSION['id']=$id;
-    signInUser($id,$remember=false,$noRedirect=false);
-    redirect('./index.php',true,2);
+    $_SESSION['id']=$id;
+    print "<meta refresh=\"index.php?token=\"".$token.">";
 }
