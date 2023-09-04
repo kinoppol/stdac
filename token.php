2e80496ae7507324c2915be3c59acede08fc51cb
kinoppol <kinoppol@hotmail.com>
2023-09-04 10:44:21 +0700

    u8

--- token.php
+++ token.php
@@ -8,6 +8,7 @@ if($token!=$chk_token){
     print "Error - token expired.";
     exit();
 }else{
+    print '<meta charset="UTF-8">';
     print "โปรดรอสักครู่..";
     $_SESSION['id_token']=$id;
     print "<meta http-equiv=\"refresh\" content=\"2;./index.php?token=".$token."\">";
