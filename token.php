70042cbcfa1d1d2cdf252a2ef416c477f3685eae
kinoppol <kinoppol@hotmail.com>
2023-09-04 09:30:26 +0700

    red

--- token.php
+++ token.php
@@ -12,8 +12,11 @@ if($token!=$chk_token){
     //require_once('system/include/config.php');
     require_once('system/library/functions.php');
     require_once('system/library/fun/user.fun.php');
+    ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
     print "โปรดรอสักครู่..";
-    $_SESSION['id']=$id;
+    $id=$_SESSION['id']=$id;
     signInUser($id,$remember=false,$noRedirect=false);
     redirect('./index.php',true,2);
 }
