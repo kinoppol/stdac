913fcb2ae555f3e04941c768b6e0a241052942ed
kinoppol <kinoppol@hotmail.com>
2025-02-13 10:24:11 +0700

    Update group_evaluate.php

--- apps/admin/gradeTransfer/group_evaluate.php
+++ apps/admin/gradeTransfer/group_evaluate.php
@@ -49,7 +49,6 @@ foreach($acts as $row){
 
     foreach($act_id as $act_row){
         $check_data=sSelectTb($systemDb,"entry_record",'*','act_id='.$act_row.' AND student_id='.sQ($row['student_id']));
-        print_r($check_data);
         $check_data=$check_data[0];
         if($check_data['entry_type']=='check'){
             $signAct++;
@@ -60,7 +59,7 @@ foreach($acts as $row){
     }else{
         $ac_percentage=$signAct/$count_ac*100;
     }
-    print $ac_percentage."<<";
+    print '('.$signAct.')'.$ac_percentage."<<";
     return array(
         'total_act'=>$count_ac,
         'checked_act'=>$signAct,
