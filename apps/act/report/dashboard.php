f35bafc2413469542141c67d6a7d6ba940a856cf
kinoppol <kinoppol@hotmail.com>
2025-02-21 15:17:07 +0700

    Update dashboard.php

--- apps/act/report/dashboard.php
+++ apps/act/report/dashboard.php
@@ -2,9 +2,9 @@
     $title="ภาพรวม";
 
     $current_semester=get_system_config("current_semester");
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 ?>
   <div class="container-fluid">
