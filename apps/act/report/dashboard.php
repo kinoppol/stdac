d52d004ba7afebf21e19fa29ed408b76fdab65b4
kinoppol <kinoppol@hotmail.com>
2025-02-21 14:44:04 +0700

    Update dashboard.php

--- apps/act/report/dashboard.php
+++ apps/act/report/dashboard.php
@@ -39,7 +39,7 @@ error_reporting(E_ALL);
                         </div>
                         
                         <div class="content">
-                            <div class="text">จำนวนกิจกรรมทั้งหมด</div>
+                            <div class="text">จำนวนกิจกรรมทั้งหมด X</div>
                             <div class="number count-to" data-from="0" data-to="<?php print $act_amount['c'];?>" data-speed="1000" data-fresh-interval="20"></div>
                         </div>
                         
